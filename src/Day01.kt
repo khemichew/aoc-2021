f1a858954f378001b6f06971f225bba9b2b138e8
Khemi <kjc20@ic.ac.uk>
2021-12-01 10:25:06 +0000

    completed day 1 part 1 and 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,45 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+  fun parseInput(input: List<String>): List<Int?> {
+    return input.map { s -> s.toIntOrNull() }
+  }
+
+  fun part1(input: List<String>): Int {
+    var ans = 0
+    val parsedInput = parseInput(input)
+    for (i in 0 until input.size - 1) {
+      if (parsedInput[i + 1]!! > parsedInput[i]!!) {
+        ans++
+      }
+    }
+    return ans
+  }
+
+  fun part2(input: List<String>): Int {
+    if (input.size <= 3) return 0
+
+    val parsedInput = parseInput(input)
+    var ans = 0
+    var windowSum = 0
+    for (i in 0..2) {
+      windowSum += parsedInput[i]!!
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    for (i in 3 until input.size) {
+      val nextWindowSum = windowSum + parsedInput[i]!! - parsedInput[i - 3]!!
+      if (nextWindowSum > windowSum) {
+        ans++
+      }
+      windowSum = nextWindowSum
     }
+    return ans
+  }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput("Day01_test")
+  check(part1(testInput) == 7)
+  check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+  val input = readInput("Day01")
+  println(part1(input))
+  println(part2(input))
 }
