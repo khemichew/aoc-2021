411df188157fd88579bbb129a030764ff5b867cf
Khemi <kjc20@ic.ac.uk>
2021-12-03 20:46:23 +0000

    updated day 1 task 1 and 2 implementation

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,45 +1,21 @@
 fun main() {
-  fun parseInput(input: List<String>): List<Int?> {
-    return input.map { s -> s.toIntOrNull() }
+  fun intervalLessThan(numbers: List<Int>, interval: Int): Int {
+    return numbers.windowed(interval).count{ xs -> xs.first() < xs.last() }
   }
 
-  fun part1(input: List<String>): Int {
-    var ans = 0
-    val parsedInput = parseInput(input)
-    for (i in 0 until input.size - 1) {
-      if (parsedInput[i + 1]!! > parsedInput[i]!!) {
-        ans++
-      }
-    }
-    return ans
+  fun part1(input: List<Int>): Int {
+    return intervalLessThan(input, 2)
   }
 
-  fun part2(input: List<String>): Int {
-    if (input.size <= 3) return 0
-
-    val parsedInput = parseInput(input)
-    var ans = 0
-    var windowSum = 0
-    for (i in 0..2) {
-      windowSum += parsedInput[i]!!
-    }
-
-    for (i in 3 until input.size) {
-      val nextWindowSum = windowSum + parsedInput[i]!! - parsedInput[i - 3]!!
-      if (nextWindowSum > windowSum) {
-        ans++
-      }
-      windowSum = nextWindowSum
-    }
-    return ans
+  fun part2(input: List<Int>): Int {
+    return intervalLessThan(input, 4)
   }
 
-  // test if implementation meets criteria from the description, like:
-  val testInput = readInput("Day01_test")
+  val testInput = readInputAsInts("Day01_test")
   check(part1(testInput) == 7)
   check(part2(testInput) == 5)
 
-  val input = readInput("Day01")
+  val input = readInputAsInts("Day01")
   println(part1(input))
   println(part2(input))
 }
